6a341a65ada680a7df513b9738853069984f4038
Gwinyai <gtchax@users.noreply.github.com>
2023-12-20 21:14:23 +0200

    removing entire styles node

--- paste.js
+++ paste.js
@@ -1,17 +1,8 @@
-/* -----------------
- Date - 20 Dec 2023
-
-  Utility functions
-    1. remove removeStyleLess - removes styleless property in objects recursively
-    2. removeStyleProperties - removes style properties  in an array recursively
-  
-*/
-
 function removeStyleLess(obj) {
   for (const prop in obj) {
     if (typeof obj[prop] === "object") {
       removeStyleLess(obj[prop]);
-    } else if (prop === "styleLess" || prop === "style") {
+    } else if (prop === "styleLess" || prop === "style" || prop === "styles") {
       delete obj[prop];
     }
   }
@@ -20,7 +11,7 @@ function removeStyleLess(obj) {
 
 function removeStyleProperties(obj) {
   for (const key in obj) {
-    if (key === "style") {
+    if (key === "style" || key === "styles") {
       delete obj[key];
     } else if (typeof obj[key] === "object") {
       removeStyleProperties(obj[key]);
@@ -29,17 +20,16 @@ function removeStyleProperties(obj) {
   return obj;
 }
 
-const init = function () {
+const onx = function () {
   document.addEventListener("paste", function (event) {
     event.preventDefault();
-
-    let pastedText = (event.clipboardData || window.clipboardData).getData(
-      "text"
-    ).setData('');
+    let pastedText = (event.clipboardData || window.clipboardData)
+      .getData("text")
+   
     const jsonObject = JSON.parse(pastedText);
     const removeInObject = removeStyleLess(jsonObject);
     const updatedText = removeStyleProperties(removeInObject);
-    event.target.value = updatedText;
+    event.target.value = JSON.stringify(updatedText);
   });
 };
-init();
+onx();
